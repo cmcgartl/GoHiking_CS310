97aa16d3053137c3f3bcfdeeecbe90b413824631
dougpete03 <dap59024@usc.edu>
2024-10-29 18:32:10 -0700

    class features implemented

--- app/src/main/java/com/example/gohiking_cs310/ReviewActivity.java
+++ app/src/main/java/com/example/gohiking_cs310/ReviewActivity.java
@@ -1,2 +1,24 @@
-package com.example.gohiking_cs310;public class ReviewActivity {
+package com.example.gohiking_cs310;
+
+public class ReviewActivity {
+    private String reviewID;
+    private String userID;
+    private String hikeID;
+    private double rating;
+    private String comments;
+
+    public ReviewActivity(String reviewID, String userID, String hikeID, double rating, String comments) {
+        this.reviewID = reviewID;
+        this.userID = userID;
+        this.hikeID = hikeID;
+        this.rating = rating;
+        this.comments = comments;
+    }
+
+    public void submitReview() {
+    }
+
+    public String getReview() {
+        return comments;
+    }
 }
--- app/src/main/java/com/example/gohiking_cs310/UserActivity.java
+++ app/src/main/java/com/example/gohiking_cs310/UserActivity.java
@@ -1,2 +1,30 @@
-package com.example.gohiking_cs310;public class UserActivity {
+package com.example.gohiking_cs310;
+import java.util.ArrayList;
+
+public class UserActivity {
+    private String userID;
+    private String name;
+    private String email;
+    private String password;
+    private ArrayList<UserActivity> friendsList;
+
+    public UserActivity(String userID, String name, String email, String password) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.friendsList = new ArrayList<>();
+    }
+
+    public void addFriend(UserActivity user) {
+        friendsList.add(user);
+    }
+
+    public void removeFriend(UserActivity user) {
+        friendsList.remove(user);
+    }
+
+    public ArrayList<UserActivity> getFriends() {
+        return friendsList;
+    }
 }
